886a269033e5e65d8ee0e0cf06162786610a9faf
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2025-08-27 16:08:56 +0530

    Arrays_part01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// // array
+
+ const myArray = [1, 2, 3];
+// console.log(myArray); // [1, 2, 3]
+
+// const myArray2 = new Array(4, 5, 6);
+// console.log(myArray2);
+
+// //array methods
+
+// myArray.push(4);
+// // myArray.pop();
+// // myArray.unshift(0);
+// // myArray.shift(9);
+// // myArray.splice(1, 1); // remove 1 element at index 1
+// // `console.log(myArray.indexOf(2)); // 1
+// // console.log(myArray.includes(3)); // true   
+
+
+// const newArray = myArray.join();
+// console.log(newArray); 
+
+// console.log(myArray);
+
+// slice, splice, concat, map, filter, reduce, forEach
+
+console.log("A",myArray)
+
+const myn1 = myArray.slice(1,2)
+console.log(myn1);
+
+console.log("B",myArray);
+
+
+const myn2 = myArray.splice(1,2)
+console.log("C",myArray);
+console.log(myn2);
+console.log("D",myArray);
+
+
