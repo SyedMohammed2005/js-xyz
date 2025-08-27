4141c6eace92a7a2c3399e46b921620819336b49
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2025-08-27 13:21:50 +0530

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,34 @@
+const name='syed'
+const repoCount=50
+
+// console.log(name+' has '+repoCount+' repositories');
+console.log(`hello my name is ${name} and i have ${repoCount} repositories`);
+
+const gameName=new String('syed ')
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__.concat);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.slice(0,3));
+console.log(gameName.slice(0));
+console.log(gameName.charAt(0));
+
+const newString=gameName.substring(0,3)
+console.log(newString);
+
+const newString2="         syed  "
+console.log(newString2);
+console.log(newString2.trim());
+
+const url="https://www.youtube.com/watch"
+console.log(url.replace('https','http'));
+
+
+
+
+
+
+
