b447010c755539ae792d68eddbe89a7957a5ddbf
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2025-08-27 20:04:50 +0530

    obj_de-struct_json_Api

---
+++ 02_basics/05_object_destructure.js
@@ -0,0 +1,28 @@
+const course={
+    coursename:"jsxyz",
+    price:"9999",
+    courseInstructor:"syed"
+
+}
+const {courseInstructor:instructor}=course
+console.log(instructor);
+
+// react destructure
+
+const navbar=({company}) =>{
+}
+navbar(company="syed")
+
+
+// {
+//     "name":"syed",
+//     "age":24 ,
+//    " coursename": "jsxyz",
+//     "price": "9999"
+// }
+
+[
+    {},
+    {},
+    {}
+]
