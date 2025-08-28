33dfd77f083e6870323f7ab8f724eeade5965101
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2025-08-28 08:17:40 +0530

    scopes

---
+++ 03_basics/03_scope.js
@@ -0,0 +1,42 @@
+// var c=50
+
+let a=300
+
+if(true)    
+{
+let   a=10
+const b=20
+console.log(a);
+
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username="syed"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+     two()
+}
+one()
+
+// +++++++++++intersting example+++++++++++++++
+
+  console.log(addone(5)); 
+function addone(num){
+    return num+1
+}
+
+   addTwo(5)
+   const addTwo=function addone(num){
+    return num+2
+   }
+ 
